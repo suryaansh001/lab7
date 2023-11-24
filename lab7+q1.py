16cedd00136124d64af00160971681eaa9c77690
suryaansh001 <141883923+suryaansh001@users.noreply.github.com>
2023-11-24 13:31:06 +0530

    Update lab7+q1.py

--- lab7+q1.py
+++ lab7+q1.py
@@ -1,2 +1,12 @@
-for i in range(ord('A'), ord('Z') + 1):
-    print(chr(i), end=' ')
+k=str(input())
+c=0
+s=0
+for i in k:
+    if ord("A")<=ord(i)<=ord("Z"):
+        c+=1
+    else:
+        s+=1
+if s>=1:
+    print("no")
+else:
+    print("yues")
